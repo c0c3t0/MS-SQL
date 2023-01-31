1cc17d705a7a85cb67cb136b7aa7ebd6ce1b12af
Stoyanka Stoichkova <98971277+c0c3t0@users.noreply.github.com>
2023-01-31 16:20:57 +0200

    Functions and Stored Procedures

--- Functions and Stored Procedures.sql
+++ Functions and Stored Procedures.sql
@@ -133,3 +133,113 @@ FROM Test
 
 
 
+--08. *Delete Employees and Departments
+CREATE OR ALTER PROC usp_DeleteEmployeesFromDepartment(@departmentId INT)
+AS
+
+
+
+
+
+
+
+
+
+--09. Find Full Name
+CREATE OR ALTER PROC usp_GetHoldersFullName
+AS
+SELECT
+	CONCAT(FirstName, ' ', LastName) AS [Full Name]
+FROM
+	AccountHolders;
+
+EXEC usp_GetHoldersFullName;
+
+
+
+--10. People with Balance Higher Than
+CREATE OR ALTER PROC usp_GetHoldersWithBalanceHigherThan(@num MONEY)
+AS
+SELECT
+	ah.FirstName AS [First Name]
+	, ah.LastName AS [Last Name]
+	--	, a.Balance
+FROM
+	AccountHolders AS ah
+JOIN Accounts AS a ON
+	ah.Id = a.AccountHolderId
+GROUP BY
+	ah.LastName
+	, ah.FirstName
+HAVING
+	@num < SUM(a.Balance);
+
+
+EXEC usp_GetHoldersWithBalanceHigherThan 75000;
+
+
+
+--11. Future Value Function
+CREATE FUNCTION ufn_CalculateFutureValue(@sum MONEY
+, @interestRate FLOAT
+, @years INT)
+RETURNS MONEY
+AS
+BEGIN
+	RETURN @sum * (POWER(1 + @interestRate, @years))
+END;
+
+
+
+--12. Calculating Interest
+CREATE OR ALTER PROC usp_CalculateFutureValueForAccount(@accountId INT, @interestRate FLOAT)
+AS
+DECLARE 
+	@years INT = 5
+SELECT
+	ah.Id AS [Account Id]
+	, ah.FirstName AS [First Name]
+	, ah.LastName AS [Last Name]
+	, a.Balance AS [Current Balance]
+	, dbo.ufn_CalculateFutureValue(a.Balance, @interestRate, @years) AS [Balance in 5 years]
+FROM
+	AccountHolders AS ah
+JOIN Accounts AS a ON
+	ah.Id = a.AccountHolderId
+WHERE
+	a.Id = @accountId;
+
+
+EXEC usp_CalculateFutureValueForAccount 1, 0.1;
+
+
+
+-- == [Part II – Queries for Diablo Database] == --
+USE Diablo;
+
+
+--13. *Cash in User Games Odd Rows
+CREATE FUNCTION ufn_CashInUsersGames(@gameName NVARCHAR(50))
+RETURNS TABLE
+AS
+RETURN(
+	SELECT
+		SUM(ocbg.Cash) AS SumCash
+	FROM
+		(
+		SELECT
+			g.Name
+			, ug.Cash
+			, ROW_NUMBER() OVER(ORDER BY ug.Cash DESC) AS rowNum
+		FROM
+			Games AS g
+		JOIN UsersGames AS ug ON
+			ug.GameId = g.Id
+		WHERE
+			Name = @gameName) AS ocbg
+	WHERE
+		ocbg.rowNum % 2 != 0);
+	
+SELECT * FROM dbo.ufn_CashInUsersGames ('Love in a mist');
+
+
