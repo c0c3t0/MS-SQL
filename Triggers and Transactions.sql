d1bba8b7ebe4853d02732a4c5d79df707c2ee1e3
Stoyanka Stoichkova <98971277+c0c3t0@users.noreply.github.com>
2023-02-02 02:01:30 +0200

    Triggers and Transactions

--- Triggers and Transactions.sql
+++ Triggers and Transactions.sql
@@ -54,3 +54,60 @@ BEGIN
 	EXEC usp_WithdrawMoney @SenderId, @Amount
 	COMMIT 
 END
+
+
+-- == [Part II - Queries for Diablo Database] == --
+USE Diablo;
+
+--06. Trigger
+
+--07. *Massive Shopping
+
+
+-- == [Part III - Queries for SoftUni Database] == --
+USE SoftUni
+
+
+--08. Employees with Three Projects
+CREATE OR ALTER PROC usp_AssignProject(@emloyeeId INT, @projectID INT)
+AS 
+BEGIN 
+	BEGIN TRANSACTION
+    DECLARE @EmployeeProjects INT
+    SET @EmployeeProjects = (SELECT COUNT(ep.ProjectID)
+                               FROM EmployeesProjects ep
+                              WHERE ep.EmployeeID = @emloyeeId)
+    IF (@EmployeeProjects >= 3)
+    BEGIN
+        RAISERROR('The employee has too many projects!', 16, 1)
+        ROLLBACK
+        RETURN
+    END
+    INSERT INTO EmployeesProjects
+          VALUES (@emloyeeId, @projectID)
+    COMMIT
+END
+
+
+
+-- 09. Delete Employees
+CREATE TABLE Deleted_Employees(
+    EmployeeId INT IDENTITY PRIMARY KEY
+    , FirstName NVARCHAR(50)
+    , LastName NVARCHAR(50)
+    , MiddleName NVARCHAR(50)
+    , JobTitle NVARCHAR(50)
+    , DepartmentId INT
+    , Salary MONEY
+);
+
+CREATE OR ALTER TRIGGER tr_OnDeletedEmployee
+ON Employees FOR DELETE
+AS
+    INSERT INTO Deleted_Employees
+    SELECT FirstName, LastName, MiddleName, JobTitle, DepartmentId, Salary
+    FROM deleted;
+
+
+
+
